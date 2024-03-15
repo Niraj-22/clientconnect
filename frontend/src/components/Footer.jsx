2aa0cdc205b9201d4d974c36e2a19b37dec64fa7
Niraj-22 <niraj22chordia@gmail.com>
2024-03-15 10:08:25 +0530

    Changed Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-around p-6">
+      <div>
+        <p className="dancing-script-font text-6xl">Client Connect</p>
+        <p>Igniting Growth Through Targeted Marketing</p>
+      </div>
+      <div className="flex flex-col ">
+        <div className="font-bold text-2xl">Location</div>
+        <div>PUNE,MAHARASHTRA</div>
+        <div className="font-bold text-2xl">Contact :</div>
+        <div className="">customersegmentpro@gmail.com</div>
+        <div className="">+91 9422571198</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
