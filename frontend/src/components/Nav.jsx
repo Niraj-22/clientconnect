a9e3a4a9c5b09cfab167e66565dc2ef0b7328f13
Niraj-22 <niraj22chordia@gmail.com>
2024-05-29 16:04:21 +0530

    Minor Changes

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -5,16 +5,16 @@ const Nav = () => {
     <div>
       <nav className="flex  justify-between">
         <NavLink
-          to="/data"
-          className="border border-blue-300 rounded-lg text-2xl hover:text-white m-2 hover:bg-blue-400 p-2"
-        >
-          DashBoard
-        </NavLink>
-        <NavLink
           to="/upload"
           className="border border-blue-300 rounded-lg text-2xl hover:text-white  hover:bg-blue-400 m-2 p-2"
         >
           Upload CSV
+        </NavLink>
+        <NavLink
+          to="/data"
+          className="border border-blue-300 rounded-lg text-2xl hover:text-white m-2 hover:bg-blue-400 p-2"
+        >
+          DashBoard
         </NavLink>
       </nav>
     </div>
